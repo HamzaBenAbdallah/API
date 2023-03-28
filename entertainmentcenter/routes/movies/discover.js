573e8193e8fc769426ad2097c656efd5d2d9a345
Hamza <hamza.ben.abdallah02@gmail.com>
2023-03-28 01:46:16 -0400

    Fix discover route

--- entertainmentcenter/routes/movies/discover.js
+++ entertainmentcenter/routes/movies/discover.js
@@ -6,31 +6,21 @@ const { EC_API_KEY, EC_MOVIE_BASE_URL } = process.env;
 
 export const getDiscover = async (req, res) => {
     const { params } = req.body;
-    const {
-        with_genres,
-        sort_by,
-        certification_country,
-        certification,
-        fromDate,
-        toDate,
-        minRating,
-        maxRating,
-    } = params;
 
     try {
         const url = `${EC_MOVIE_BASE_URL}discover/movie`;
 
         const { data } = await axios(url, {
             params: {
-                EC_API_KEY: EC_API_KEY,
-                with_genres,
-                sort_by,
-                certification_country,
-                certification,
-                "primary_release_date.gte": fromDate,
-                "primary_release_date.lte": toDate,
-                "vote_average.gte": minRating,
-                "vote_average.lte": maxRating,
+                api_key: EC_API_KEY,
+                with_genres: params?.with_genres || "",
+                sort_by: params?.sort_by || "popularity.desc",
+                certification_country: params?.certification_country || "US",
+                certification: params?.certification || "",
+                "primary_release_date.gte": params?.fromDate || "",
+                "primary_release_date.lte": params?.toDate || "",
+                "vote_average.gte": params?.minRating || 0,
+                "vote_average.lte": params?.maxRating || 10,
             },
         });
         res.status(200).json(data.results);
